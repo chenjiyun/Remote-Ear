aba7fec01930bc5ca45ffcdc618f4b8056adb40f
chenjiyun <aecfyn@qq.com>
2015-12-15 17:06:28 +0800

    Create XHRemoteEarServer.h

---
+++ XHRemoteEarServer.h
@@ -0,0 +1,29 @@
+/
+//  XHRemoteServer.h
+//  XHRemoteEar
+//
+//  Created by 陈小黑 on 15/10/24.
+//  Copyright © 2015年 XH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XHRemoteEarServer : NSObject <NSStreamDelegate>
+
+@property (nonatomic,strong) NSOutputStream *serverOutputStream;
+@property (nonatomic,strong) NSInputStream *serverInputStream;
+@property (nonatomic)CFSocketRef myipv4cfsock;
+@property (nonatomic)CFRunLoopSourceRef socketsource;
+@property (nonatomic,strong)  NSMutableData *data;
+@property (nonatomic,copy) void (^displayMessageBlock)(NSData *dat);
+@property (nonatomic) BOOL connectFlag;
+
+
+-(void)startListen;
+
+-(void)sendMessage:(NSString *)text;
+
+-(void)sendAudio:(NSData *)d;
+-(void)stopListen;
+
+@end
